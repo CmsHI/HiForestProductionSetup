a717809cdc87d05312031bf198480d2c573ed356
R. Alex Barbieri <luck@mit.edu>
2013-09-10 20:38:45 +0200

    Switch to CVS HEAD for muon algos

--- setup_hiForest2_44xIN53x_git.sh
+++ setup_hiForest2_44xIN53x_git.sh
@@ -9,7 +9,7 @@ cvs co -r hi44X_03                 HeavyIonsAnalysis/Configuration
 
 # Latest hiSelectedTrack (= hiGoodTightTrack)
 cvs co -r hi44X_03                 RecoHI/HiTracking
-cvs co -r hi44X_03                 RecoHI/HiMuonAlgos
+cvs co -r HEAD                     RecoHI/HiMuonAlgos
 cvs co -r hi44X_03                 RecoHI/HiJetAlgos
 cvs co -r hi44X_03                 RecoHI/HiCentralityAlgos
 cvs co -r hi44X_03                 RecoHI/HiEvtPlaneAlgos
